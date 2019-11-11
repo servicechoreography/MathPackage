72a664948c8076cd47bb11c75ac0cd4629ea243d
servicechoreography <32399774+servicechoreography@users.noreply.github.com>
2019-11-11 09:33:58 -0500

    uploaded files

---
+++ library/library.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace library
+{
+  public class Start
+  {
+    public int Add(int a, int b) 
+    {
+        return a + b;
+    }
+
+    public int Sub(int a, int b)
+    {
+      return a - b;
+    }
+  }
+}
